cd3a25c05a5c0353fb44c86e813a10fe86d9f9d9
Fiona <fcro0003@student.monash.edu>
2021-05-05 04:10:27 +1000

    Initial Commit

---
+++ HOUSEM8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HOUSEM8
+//
+//  Created by Fiona Crowe on 5/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
